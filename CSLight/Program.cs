39a2649508df9966ce17c533a429f194bae92e23
Dubdub <Dubdub@DESKTOP-1UHC7HM>
2022-05-17 21:01:49 +0300

    Добавьте файлы проекта.

---
+++ CSLight/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, мир!");
+            Console.ReadKey();
+            //домашнее задание:
+            
+            int speedOfBlueCar = 150;
+            float temperatureInside = 23.8f;
+            byte ageOfCharacter1 = 20;
+            byte ageOfCharacter2 = 27;
+            sbyte temperatureOutside = -12;                                          
+            int speedOfRedCar = 60;
+            char blueCar = 'b';
+            char redCar = 'r';
+            string finishPhrase = "Да, мать вашу!";                                     
+            char character1 = '1';
+            char character2 = '2';
+ }
+    }
+}
