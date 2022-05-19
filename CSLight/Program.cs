bcc7da91b70fa130a7f44f83e2da5618d64a80d4
DmitriyLosev8 <dmitriylosev8@gmail.com>
2022-05-19 11:41:53 +0300

    Homework3

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -10,26 +10,13 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Привет, мир!");
-            Console.ReadKey();
-            //домашнее задание:
+            // домашнее задание конкатинация
             
-            int speedOfBlueCar = 150;
-            float temperatureInside = 23.8f;
-            byte ageOfCharacter1 = 20;
-            byte ageOfCharacter2 = 27;
-            sbyte temperatureOutside = -12;                                          
-            int speedOfRedCar = 60;
-            char blueCar = 'b';
-            char redCar = 'r';
-            string finishPhrase = "Да, мать вашу!";                                     
-            char character1 = '1';
-            char character2 = '2';
-            //добавил недостающие переменные
-           
-            bool isFinishFirst = false;
-            short moneyAfterLosing = -150;
-            double procentChanceToWin = 78.4568944226549;
+            string whatIsYourName = "Алексей";
+            int howOldAreYou = 21;
+            string whatIsYourZodiak = "водолей";
+            string whereDoYouWork = "на заводе";
+            Console.WriteLine("Вас зовут " + whatIsYourName + ", вам " + howOldAreYou + " год, вы " + whatIsYourZodiak + " и работаете " + whereDoYouWork + ".");
         }
     }
 }
