3a065bdf0dfda7ff0df45fcf02258d68a60278ab
DmitriyLosev8 <dmitriylosev8@gmail.com>
2022-05-26 21:57:41 +0300

    homework

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -9,17 +9,21 @@ namespace CSLight
     internal class Program
     {
         static void Main(string[] args)
-        {   // домашнее задание поликлиника:
+        {   
+            // домашнее задание поменять местами значения переменных:
 
-            int minutesOfWaiting = 10;
-            int amoutnOfGrandmothers;
-            int timeToWait;
-
-            Console.WriteLine("Здравствуйте, я помогу вам определить сколько Вам ждать в очереди. Сколько вы видите перед собой старушек?");
-            amoutnOfGrandmothers = Convert.ToInt32(Console.ReadLine());
-
-            timeToWait = amoutnOfGrandmothers * minutesOfWaiting;
-            Console.WriteLine("Судя по моим подсчётам вам придётся ждать " + timeToWait + " минут, удачи!");
+            string name = "Волочкова";
+            string lastName = "Алёна";
+            Console.WriteLine("Ваше имя - " + name);
+            Console.WriteLine("Ваша фамилия - " + lastName);
+            name = lastName;
+            if (name == "Алёна")
+            {
+                lastName = "Волочкова";
+            }
+            Console.WriteLine("Данные были не верны, вот верные данные:");
+            Console.WriteLine("Ваше имя - " + name);
+            Console.WriteLine("Ваша фамилия - " + lastName);
         }
     }
 }
