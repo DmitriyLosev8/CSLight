36004abbbd8dde8f4cffd1c82f851600e04d56aa
DmitriyLosev8 <dmitriylosev8@gmail.com>
2022-05-26 16:59:01 +0300

    homework

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -13,10 +13,13 @@ namespace CSLight
             // домашнее задание картинки: у нас есть 52 картинке в альбоме, выводятся по 3 ряда, надо понять сколько полностью заполненных рядов можно будет вывести,
             // и сколько картинок будет сверх меры.
 
-            int fullSeriesOfPictures = 52 / 3;
-            int exraPictures = 52 % 7;
+            int albumOfPictures = 52;
+            int seriaOfPictures = 3;
+            int fullSeriesOfPictures = albumOfPictures / seriaOfPictures;
+            int extraPictures = albumOfPictures % seriaOfPictures;
             Console.WriteLine(fullSeriesOfPictures);
-            Console.WriteLine(exraPictures);
+            Console.WriteLine(extraPictures);
+            
         }
     }
 }
