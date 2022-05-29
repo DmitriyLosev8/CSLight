931f29d5734e19ddd7fa8dbe9f69fc3871236dd7
DmitriyLosev8 <dmitriylosev8@gmail.com>
2022-05-29 23:11:15 +0300

    программа под паролем

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -10,15 +10,26 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            //домашнее задание: последовательность. У нас есть неделя обучения и полный курс обучения по дням. 
-            //Использовал цикл for, потому что есть чёткие парраметры цикла и чёткий шаг.
-            
-            int weekOfStudy = 7;
-            int fullCourseOfStudy = 100;
-            
-            for (int i = 5; i <= fullCourseOfStudy; i+= weekOfStudy)
+            //домашнее задание "программа под паролем":
+
+            string password = "123456";
+            string userInput;
+            int tryCount = 3;
+
+            for (int i = 0; i < tryCount; i++)
             {
-                Console.WriteLine(i);
+                Console.WriteLine("Введите пароль, чтобы узнать страшную тайну:");
+                userInput = Console.ReadLine();
+
+                if (userInput == password)
+                {
+                    Console.WriteLine("Вы открыли страшную тайну: Бабы Яги не существует!");
+                    break;
+                }
+                else if (userInput != password)
+                {
+                    Console.WriteLine("Неа, пароль не верный, введите ещё раз");
+                }
             }
         }
     }
