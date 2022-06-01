d790ea4460c076f246e718510ec5522e481b9731
DmitriyLosev8 <dmitriylosev8@gmail.com>
2022-06-01 23:28:11 +0300

    конвертер валют

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -10,26 +10,173 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            // домашнее задание: сумма чисел
+            // домашнее задание: конвертер валют
 
-            int lowerNumber = 0;
-            int upperNumber = 101;
-            int lowerMultipleNumber = 3;
-            int upperMultipleNumber = 5;
-            int sum = 0;
+            int eurToRub = 70;
+            int usdToRub = 60;
+            float rubToEur = 0.014f;
+            float rubToUsd = 0.016f;
+            float eurToUsd = 1.166f;
+            float usdToEur = 0.857f;
 
-            Random random = new Random();
-            int number = random.Next(lowerNumber, upperNumber);
-            Console.WriteLine(number);
+            float rub;
+            float eur;
+            float usd;
+            float currencyCount;
+            string userInput;
 
-            for (int i = 0; i <= number; i ++)
+            Console.WriteLine("Это конвертер валют, вы можете обменять между собой рубли, доллары и евро!");
+            Console.WriteLine("Сколько у вас рублей?");
+            rub = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Сколько у вас евро?");
+            eur = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Сколько у вас долларов?");
+            usd = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Чтобы обменять рубли на евро, нажмите 1:\nЧтобы обменять рубли на доллары, нажмите 2:");
+            Console.WriteLine("Чтобы обменять евро на рубли, нажмите 3:\nЧтобы обменять евро на доллары, нажмите 4:");
+            Console.WriteLine("Чтобы обменять доллары на рубли, нажмите 5:\nЧтобы обменять доллары на евро, нажмите 6:");
+            Console.WriteLine("Чтобы завершить программу, нажмите 8");
+            userInput = Console.ReadLine();
+
+            while (userInput != "8")
+
             {
-                if (i % lowerMultipleNumber == 0 || i % upperMultipleNumber == 0)
+                switch (userInput)
                 {
-                    sum += i;
-                    Console.WriteLine(sum);
-                }      
+                    case "1":
+
+                        Console.WriteLine("Обмен рублей на евро.\nСколько вы хотите обменять?");
+                        currencyCount = Convert.ToSingle(Console.ReadLine());
+                        if (rub >= currencyCount)
+                        {
+                            rub -= currencyCount;
+                            eur += currencyCount * rubToEur;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Недопустимое колличество денег");
+                        }
+
+                        Console.WriteLine("Ваш баланс: " + rub + " рублей, " + eur + " евро и " + usd + " долларов.");
+                        Console.WriteLine("Чтобы обменять рубли на евро, нажмите 1:\nЧтобы обменять рубли на доллары, нажмите 2:");
+                        Console.WriteLine("Чтобы обменять евро на рубли, нажмите 3:\nЧтобы обменять евро на доллары, нажмите 4:");
+                        Console.WriteLine("Чтобы обменять доллары на рубли, нажмите 5:\nЧтобы обменять доллары на евро, нажмите 6:");
+                        Console.WriteLine("Чтобы завершить программу, нажмите 8");
+                        userInput = Console.ReadLine();
+                        break;
+
+                    case "2":
+
+                        Console.WriteLine("Обмен рублей на доллары.\nСколько вы хотите обменять?");
+                        currencyCount = Convert.ToSingle(Console.ReadLine());
+                        if (rub >= currencyCount)
+                        {
+                            rub -= currencyCount;
+                            usd += currencyCount * rubToUsd;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Недопустимое колличество денег");
+                        }
+
+                        Console.WriteLine("Ваш баланс: " + rub + " рублей, " + eur + " евро и " + usd + " долларов.");
+                        Console.WriteLine("Чтобы обменять рубли на евро, нажмите 1:\nЧтобы обменять рубли на доллары, нажмите 2:");
+                        Console.WriteLine("Чтобы обменять евро на рубли, нажмите 3:\nЧтобы обменять евро на доллары, нажмите 4:");
+                        Console.WriteLine("Чтобы обменять доллары на рубли, нажмите 5:\nЧтобы обменять доллары на евро, нажмите 6:");
+                        Console.WriteLine("Чтобы завершить программу, нажмите 8");
+                        userInput = Console.ReadLine();
+                        break;
+
+                    case "3":
+
+                        Console.WriteLine("Обмен евро на рубли.\nСколько вы хотите обменять?");
+                        currencyCount = Convert.ToSingle(Console.ReadLine());
+                        if (eur >= currencyCount)
+                        {
+                            eur -= currencyCount;
+                            rub += currencyCount * eurToRub;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Недопустимое колличество денег");
+                        }
+
+                        Console.WriteLine("Ваш баланс: " + rub + " рублей, " + eur + " евро и " + usd + " долларов.");
+                        Console.WriteLine("Чтобы обменять рубли на евро, нажмите 1:\nЧтобы обменять рубли на доллары, нажмите 2:");
+                        Console.WriteLine("Чтобы обменять евро на рубли, нажмите 3:\nЧтобы обменять евро на доллары, нажмите 4:");
+                        Console.WriteLine("Чтобы обменять доллары на рубли, нажмите 5:\nЧтобы обменять доллары на евро, нажмите 6:");
+                        Console.WriteLine("Чтобы завершить программу, нажмите 8");
+                        userInput = Console.ReadLine();
+                        break;
+
+                    case "4":
+
+                        Console.WriteLine("Обмен евро на доллары.\nСколько вы хотите обменять?");
+                        currencyCount = Convert.ToSingle(Console.ReadLine());
+                        if (eur >= currencyCount)
+                        {
+                            eur -= currencyCount;
+                            usd += currencyCount * eurToUsd;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Недопустимое колличество денег");
+                        }
+
+                        Console.WriteLine("Ваш баланс: " + rub + " рублей, " + eur + " евро и " + usd + " долларов.");
+                        Console.WriteLine("Чтобы обменять рубли на евро, нажмите 1:\nЧтобы обменять рубли на доллары, нажмите 2:");
+                        Console.WriteLine("Чтобы обменять евро на рубли, нажмите 3:\nЧтобы обменять евро на доллары, нажмите 4:");
+                        Console.WriteLine("Чтобы обменять доллары на рубли, нажмите 5:\nЧтобы обменять доллары на евро, нажмите 6:");
+                        Console.WriteLine("Чтобы завершить программу, нажмите 8");
+                        userInput = Console.ReadLine();
+                        break;
+
+                    case "5":
+
+                        Console.WriteLine("Обмен доллары на рубли.\nСколько вы хотите обменять?");
+                        currencyCount = Convert.ToSingle(Console.ReadLine());
+                        if (usd >= currencyCount)
+                        {
+                            usd -= currencyCount;
+                            rub += currencyCount * usdToRub;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Недопустимое колличество денег");
+                        }
+
+                        Console.WriteLine("Ваш баланс: " + rub + " рублей, " + eur + " евро и " + usd + " долларов.");
+                        Console.WriteLine("Чтобы обменять рубли на евро, нажмите 1:\nЧтобы обменять рубли на доллары, нажмите 2:");
+                        Console.WriteLine("Чтобы обменять евро на рубли, нажмите 3:\nЧтобы обменять евро на доллары, нажмите 4:");
+                        Console.WriteLine("Чтобы обменять доллары на рубли, нажмите 5:\nЧтобы обменять доллары на евро, нажмите 6:");
+                        Console.WriteLine("Чтобы завершить программу, нажмите 8");
+                        userInput = Console.ReadLine();
+                        break;
+
+                    case "6":
+
+                        Console.WriteLine("Обмен доллары на евро.\nСколько вы хотите обменять?");
+                        currencyCount = Convert.ToSingle(Console.ReadLine());
+                        if (usd >= currencyCount)
+                        {
+                            usd -= currencyCount;
+                            eur += currencyCount * usdToEur;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Недопустимое колличество денег");
+                        }
+
+                        Console.WriteLine("Ваш баланс: " + rub + " рублей, " + eur + " евро и " + usd + " долларов.");
+                        Console.WriteLine("Чтобы обменять рубли на евро, нажмите 1:\nЧтобы обменять рубли на доллары, нажмите 2:");
+                        Console.WriteLine("Чтобы обменять евро на рубли, нажмите 3:\nЧтобы обменять евро на доллары, нажмите 4:");
+                        Console.WriteLine("Чтобы обменять доллары на рубли, нажмите 5:\nЧтобы обменять доллары на евро, нажмите 6:");
+                        Console.WriteLine("Чтобы завершить программу, нажмите 8");
+                        userInput = Console.ReadLine();
+                        break;
+                }
             }
-        }   
+        }
     }
 }
