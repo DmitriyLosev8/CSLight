5ab8c410339d97127df42e2dc668ea69a8eae242
DmitriyLosev8 <dmitriylosev8@gmail.com>
2022-06-06 18:52:59 +0300

    консольное меню

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -10,39 +10,69 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            //домашнее задание: скобочное выражение: 
+            //домашнее задание: консольное меню:
 
-            string stringToChek = "'(' и ')'";
-            int deepOfBracket = 0;
-            Console.WriteLine(stringToChek);
+            string name;
+            string password;
+            int daysInYear = 365;
+            int daysBeforeNewYear;
+            int minimumDays = 1;
+            Random random = new Random();
+            int daysToRun = random.Next(minimumDays, daysInYear);
+            int userInput;
+            int color = 1;
+            int anotherColor = 3;
+            Random randomOfColor = new Random();
+            int colorOfFuture = randomOfColor.Next(color, anotherColor);
 
-            for (int i = 0; i < stringToChek.Length; i++)
-            {
+            Console.WriteLine("                                     ДОБРО ПОЖАЛОВАТЬ В КОНСОЛЬНОЕ МЕНЮ");
+            Console.WriteLine("Чтобы задать имя, нажмите - 1\nЧтобы задать пароль, нажмите - 2\nЧтобы узнать сколько дней подряд вам надо бегать (непредсказуемо), нажмите - 3" +
+                "\n" + "Чтобы узнать сколько дней осталось до Нового Года, нажмите - 4\nЕсли вы хотите знать, какой цвет будет популярным через 10 лет, нажмите - 5" +
+                "\nЧтобы выйти, нажмите - 6");
+            userInput = Convert.ToInt32(Console.ReadLine());
 
-                if (stringToChek[i] == '(')
+            while (userInput != 6)
+            {  
+                switch (userInput)
                 {
-                    deepOfBracket += 1;
+                    case 1:
+                        Console.WriteLine("Введите Ваше имя:");
+                        name = Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.WriteLine("Введите пароль:");
+                        password = Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.WriteLine("По нашим подсчётам, чтобы Вы были в тонусе, Вам надо бегать - " + daysToRun + " дней подряд. Удачи!");
+                        break;
+                    case 4:
+                        Console.WriteLine("Какой по счёту сегодня день в году?");
+                        userInput = Convert.ToInt32(Console.ReadLine());
+                        daysBeforeNewYear = daysInYear - userInput;
+                        Console.WriteLine(daysBeforeNewYear + " - столько дней осталось до Нового Года.");
+                        break;
+                    case 5:
+
+                        if (colorOfFuture == 1)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Предполагаемый нами популярный цвет через десять лет - зелёный");
+                        }
+                        if (colorOfFuture == 2)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Предполагаемый нами популярный цвет через десять лет - красный");
+                        }
+                        if (colorOfFuture == 3)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.WriteLine("Предполагаемый нами популярный цвет через десять лет - синий");
+                        }
+                        break;
                 }
-
-                if (stringToChek[i] == ')')
-                {
-                    deepOfBracket -= 1;
-                }
-
-                if (deepOfBracket < 0)
-                {
-                    break;
-                }
-            }
-
-            if (deepOfBracket == 0)
-            {
-                Console.WriteLine("Строка корректна");
-            }
-
-            else
-            {
-                Console.WriteLine("Строка не корректна");
+                Console.ForegroundColor = ConsoleColor.White;
+                userInput = Convert.ToInt32(Console.ReadLine());
             }
         }
     }
