46bccecb4dbe3fcf8d1f556c252c0efae617197b
DmitriyLosev8 <dmitriylosev8@gmail.com>
2022-06-07 19:10:35 +0300

    бой с боссом:

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -10,69 +10,155 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            //домашнее задание: консольное меню:
+            //домашнее задание: бой с боссом:
 
-            string name;
-            string password;
-            int daysInYear = 365;
-            int daysBeforeNewYear;
-            int minimumDays = 1;
+            int healthOfHero = 400;
+            int healthOfBoss = 600;
+            int bossDamage = 20;
+            int minimumHeroDamage = 7;
+            int maximumHeroDamage = 15;
             Random random = new Random();
-            int daysToRun = random.Next(minimumDays, daysInYear);
-            int userInput;
-            int color = 1;
-            int anotherColor = 3;
-            Random randomOfColor = new Random();
-            int colorOfFuture = randomOfColor.Next(color, anotherColor);
+            int powerOfAirDamage = random.Next(minimumHeroDamage, maximumHeroDamage);
+            int powerOfEarthHealth = 100;
+            int powerOfEarthDamage = 20;
+            int powerOfWaterHealth = 150;
+            int powerOfWaterDamage = 10;
+            int ultimatePowerHealth = 50;
+            int ultimatePowerDamage = 100;
+            int healthForUltimatePower = 60;
+            int healthForPowerOfEarth = 300;
+            int healthForPowerOfWater = 150;
+            string userInput;
 
-            Console.WriteLine("                                     ДОБРО ПОЖАЛОВАТЬ В КОНСОЛЬНОЕ МЕНЮ");
-            Console.WriteLine("Чтобы задать имя, нажмите - 1\nЧтобы задать пароль, нажмите - 2\nЧтобы узнать сколько дней подряд вам надо бегать (непредсказуемо), нажмите - 3" +
-                "\n" + "Чтобы узнать сколько дней осталось до Нового Года, нажмите - 4\nЕсли вы хотите знать, какой цвет будет популярным через 10 лет, нажмите - 5" +
-                "\nЧтобы выйти, нажмите - 6");
-            userInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("                                  Впереди Вас ждёт финальное сражение! Босс - очень силён, но у Вас есть шансы!");
+            Console.WriteLine("Для этого Вам нужно чётко расчитать стратегию ведения боя. У Босса " + healthOfBoss + " хп и " + bossDamage + " урона.");
+            Console.WriteLine("У Вас же " + healthOfHero + " хп и четыре заклинания:\n\nСила Воздуха - Ваш урон будет от " + minimumHeroDamage + " до " + maximumHeroDamage +
+                ", нажмите 1, чтобы применить\n\nСила Земли - Ваш урон будет - " + powerOfEarthDamage + ", но отнимется - " + powerOfEarthHealth + " хп," + "\n" +
+                "(МОЖНО ПРИМЕНИТЬ, ТОЛЬКО ЕСЛИ У ВАС БОЛЬШЕ ХП ЧЕМ - " + healthForPowerOfEarth + ", нажмите 2, чтобы применить\n\n" +
+                "Сила Воды - Вам прибавится - " + powerOfWaterHealth + " хп, но урон будет - " + powerOfWaterDamage + ",\n" +
+                "(МОЖНО ПРИМЕНИТЬ, ТОЛЬКО ЕСЛИ У ВАС ХП НИЖЕ ЧЕМ - " + healthForPowerOfWater + ", нажмите 3, чтобы применить\n\n" +
+                "Абсолютная Сила  - Ваш урон будет - " + ultimatePowerDamage + ", а хп увеличится на - " + ultimatePowerHealth + "\n" +
+                "(МОЖНО ПРИМЕНИТЬ, ТОЛЬКО КОГДА У ВАС - " + healthForUltimatePower + " ХП ИЛИ НИЖЕ). Нажмите 4, чтобы применить\n\n" +
+                "                                                        ДА НАЧНЁТСЯ БИТВА!");
+            userInput = Console.ReadLine();
 
-            while (userInput != 6)
-            {  
+            while (healthOfHero > 0 && healthOfBoss > 0)
+            {
                 switch (userInput)
                 {
-                    case 1:
-                        Console.WriteLine("Введите Ваше имя:");
-                        name = Console.ReadLine();
-                        break;
-                    case 2:
-                        Console.WriteLine("Введите пароль:");
-                        password = Console.ReadLine();
-                        break;
-                    case 3:
-                        Console.WriteLine("По нашим подсчётам, чтобы Вы были в тонусе, Вам надо бегать - " + daysToRun + " дней подряд. Удачи!");
-                        break;
-                    case 4:
-                        Console.WriteLine("Какой по счёту сегодня день в году?");
-                        userInput = Convert.ToInt32(Console.ReadLine());
-                        daysBeforeNewYear = daysInYear - userInput;
-                        Console.WriteLine(daysBeforeNewYear + " - столько дней осталось до Нового Года.");
-                        break;
-                    case 5:
+                    case "1":
+                        healthOfBoss -= powerOfAirDamage;
+                        healthOfHero -= bossDamage;
+                        Console.WriteLine("Герой - " + healthOfHero + " хп                                  Босс - " + healthOfBoss + " хп");
 
-                        if (colorOfFuture == 1)
+                        if (healthOfHero <= 0 && healthOfBoss <= 0)
                         {
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            Console.WriteLine("Предполагаемый нами популярный цвет через десять лет - зелёный");
+                            Console.WriteLine("Так бывает. Ничья!");
                         }
-                        if (colorOfFuture == 2)
+
+                        else if (healthOfHero <= 0)
                         {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("Предполагаемый нами популярный цвет через десять лет - красный");
+                            Console.WriteLine("Не в этот раз. Победил Босс!");
                         }
-                        if (colorOfFuture == 3)
+
+                        else if (healthOfBoss <= 0)
                         {
-                            Console.ForegroundColor = ConsoleColor.Blue;
-                            Console.WriteLine("Предполагаемый нами популярный цвет через десять лет - синий");
+                            Console.WriteLine("Босс повержен! Победил Герой!");
+                        }
+                        break;
+                    case "2":
+
+                        if (healthOfHero > healthForPowerOfEarth)
+                        {
+                            healthOfHero -= powerOfEarthHealth;
+                            healthOfBoss -= powerOfEarthDamage;
+                            healthOfHero -= bossDamage;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Сейчас невозможно применить Силу Земли, у Вас хп меньше чем - " + healthForPowerOfEarth);
+                        }
+                        Console.WriteLine("Герой - " + healthOfHero + " хп                                  Босс - " + healthOfBoss + " хп");
+
+                        if (healthOfHero <= 0 && healthOfBoss <= 0)
+                        {
+                            Console.WriteLine("Так бывает. Ничья!");
+                        }
+
+                        else if (healthOfHero <= 0)
+                        {
+                            Console.WriteLine("Не в этот раз. Победил Босс!");
+                        }
+
+                        else if (healthOfBoss <= 0)
+                        {
+                            Console.WriteLine("Босс повержен! Победил Герой!");
+                        }
+                        break;
+                    case "3":
+
+                        if (healthOfHero < healthForPowerOfWater)
+                        {
+                            healthOfHero += powerOfWaterHealth;
+                            healthOfBoss -= powerOfWaterDamage;
+                            healthOfHero -= bossDamage;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Сейчас невозможно применить Силу Воды, у Вас хп больше чем - " + healthForPowerOfWater);
+                        }
+
+                        Console.WriteLine("Герой - " + healthOfHero + " хп                                  Босс - " + healthOfBoss + " хп");
+
+                        if (healthOfHero <= 0 && healthOfBoss <= 0)
+                        {
+                            Console.WriteLine("Так бывает. Ничья!");
+                        }
+
+                        else if (healthOfHero <= 0)
+                        {
+                            Console.WriteLine("Не в этот раз. Победил Босс!");
+                        }
+
+                        else if (healthOfBoss <= 0)
+                        {
+                            Console.WriteLine("Босс повержен! Победил Герой!");
+                        }
+                        break;
+                    case "4":
+
+                        if (healthOfHero <= healthForUltimatePower)
+                        {
+                            healthOfHero += ultimatePowerHealth;
+                            healthOfBoss -= ultimatePowerDamage;
+                            healthOfHero -= bossDamage;
+                        }
+                        
+                        else
+                        {
+                            Console.WriteLine("Сейчас невозможно применить Абсолютную Силу, у Вас хп больше чем - " + healthForUltimatePower);
+                        }
+                        Console.WriteLine("Герой - " + healthOfHero + " хп                                  Босс - " + healthOfBoss + " хп");
+
+                        if (healthOfHero <= 0 && healthOfBoss <= 0)
+                        {
+                            Console.WriteLine("Так бывает. Ничья!");
+                        }
+
+                        else if (healthOfHero <= 0)
+                        {
+                            Console.WriteLine("Не в этот раз. Победил Босс!");
+                        }
+
+                        else if (healthOfBoss <= 0)
+                        {
+                            Console.WriteLine("Босс повержен! Победил Герой!");
                         }
                         break;
                 }
-                Console.ForegroundColor = ConsoleColor.White;
-                userInput = Convert.ToInt32(Console.ReadLine());
+                userInput = Console.ReadLine();
             }
         }
     }
