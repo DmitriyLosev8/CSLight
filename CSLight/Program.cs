871bbdb46c61a00ee3a1fe8a0672dad5ccfd1ab5
DmitriyLosev8 <dmitriylosev8@gmail.com>
2022-06-10 11:00:26 +0300

    локальные максимумы

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -10,106 +10,34 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            //домашнее задание: бой с боссом:
+            // дамашнее задание: локальные максимумы: 
 
-            int healthOfHero = 400;
-            int healthOfBoss = 600;
-            int bossDamage = 20;
-            int minimumHeroDamage = 7;
-            int maximumHeroDamage = 15;
+            int[] array = new int[30];
             Random random = new Random();
-            int powerOfAirDamage = random.Next(minimumHeroDamage, maximumHeroDamage);
-            int powerOfEarthHealth = 100;
-            int powerOfEarthDamage = 20;
-            int powerOfWaterHealth = 150;
-            int powerOfWaterDamage = 10;
-            int ultimatePowerHealth = 50;
-            int ultimatePowerDamage = 100;
-            int healthForUltimatePower = 60;
-            int healthForPowerOfEarth = 300;
-            int healthForPowerOfWater = 150;
-            string userInput;
+            int minimalNumber = 0;
+            int maximalNumber = 100;
+            int minimalBorder = 0;
+            int maximalBorder = 29;
 
-            Console.WriteLine("                                  Впереди Вас ждёт финальное сражение! Босс - очень силён, но у Вас есть шансы!");
-            Console.WriteLine("Для этого Вам нужно чётко расчитать стратегию ведения боя. У Босса " + healthOfBoss + " хп и " + bossDamage + " урона.");
-            Console.WriteLine("У Вас же " + healthOfHero + " хп и четыре заклинания:\n\nСила Воздуха - Ваш урон будет от " + minimumHeroDamage + " до " + maximumHeroDamage +
-                ", нажмите 1, чтобы применить\n\nСила Земли - Ваш урон будет - " + powerOfEarthDamage + ", но отнимется - " + powerOfEarthHealth + " хп," + "\n" +
-                "(МОЖНО ПРИМЕНИТЬ, ТОЛЬКО ЕСЛИ У ВАС БОЛЬШЕ ХП ЧЕМ - " + healthForPowerOfEarth + ", нажмите 2, чтобы применить\n\n" +
-                "Сила Воды - Вам прибавится - " + powerOfWaterHealth + " хп, но урон будет - " + powerOfWaterDamage + ",\n" +
-                "(МОЖНО ПРИМЕНИТЬ, ТОЛЬКО ЕСЛИ У ВАС ХП НИЖЕ ЧЕМ - " + healthForPowerOfWater + ", нажмите 3, чтобы применить\n\n" +
-                "Абсолютная Сила  - Ваш урон будет - " + ultimatePowerDamage + ", а хп увеличится на - " + ultimatePowerHealth + "\n" +
-                "(МОЖНО ПРИМЕНИТЬ, ТОЛЬКО КОГДА У ВАС - " + healthForUltimatePower + " ХП ИЛИ НИЖЕ). Нажмите 4, чтобы применить\n\n" +
-                "                                                        ДА НАЧНЁТСЯ БИТВА!");
-
-            while (healthOfHero > 0 && healthOfBoss > 0)
+            for (int i = 0; i < array.Length; i++)
             {
-                userInput = Console.ReadLine();
-                Console.WriteLine("Герой - " + healthOfHero + " хп                                  Босс - " + healthOfBoss + " хп");
+                array[i] = random.Next(minimalNumber, maximalNumber);
 
-                switch (userInput)
+                if (i == minimalBorder || i == maximalBorder)
                 {
-                    case "1":
-                        healthOfBoss -= powerOfAirDamage;
-                        healthOfHero -= bossDamage;
-                        break;
-                    case "2":
-
-                        if (healthOfHero > healthForPowerOfEarth)
-                        {
-                            healthOfHero -= powerOfEarthHealth;
-                            healthOfBoss -= powerOfEarthDamage;
-                            healthOfHero -= bossDamage;
-                        }
-
-                        else
-                        {
-                            Console.WriteLine("Сейчас невозможно применить Силу Земли, у Вас хп меньше чем - " + healthForPowerOfEarth);
-                        }
-                        break;
-                    case "3":
-
-                        if (healthOfHero < healthForPowerOfWater)
-                        {
-                            healthOfHero += powerOfWaterHealth;
-                            healthOfBoss -= powerOfWaterDamage;
-                            healthOfHero -= bossDamage;
-                        }
-
-                        else
-                        {
-                            Console.WriteLine("Сейчас невозможно применить Силу Воды, у Вас хп больше чем - " + healthForPowerOfWater);
-                        }
-                        break;
-                    case "4":
-
-                        if (healthOfHero <= healthForUltimatePower)
-                        {
-                            healthOfHero += ultimatePowerHealth;
-                            healthOfBoss -= ultimatePowerDamage;
-                            healthOfHero -= bossDamage;
-                        }
-                        
-                        else
-                        {
-                            Console.WriteLine("Сейчас невозможно применить Абсолютную Силу, у Вас хп больше чем - " + healthForUltimatePower);
-                        } 
-                        break;
+                    Console.WriteLine("\nЛокальный максимум:");
+                    Console.Write(array[i] + "\n");
                 }
                 
-                if (healthOfHero <= 0 && healthOfBoss <= 0)
+                if (i > minimalBorder && i < maximalBorder)
                 {
-                    Console.WriteLine("Так бывает. Ничья!");
-                }
 
-                else if (healthOfHero <= 0)
-                {
-                    Console.WriteLine("Не в этот раз. Победил Босс!");
+                    if (array[i] > array[i - 1] && array[i] > array[i + 1])
+                    {
+                        Console.WriteLine("\nЛокальный максимум:");
+                        Console.WriteLine(array[i]);
+                    }
                 }
-
-                else if (healthOfBoss <= 0)
-                {
-                    Console.WriteLine("Босс повержен! Победил Герой!");
-                } 
             }
         }
     }
