a41298d73c8cfdabafc159f5cb302d56f5526936
DmitriyLosev8 <dmitriylosev8@gmail.com>
2022-06-10 12:22:27 +0300

    локальные максимумы

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -12,7 +12,7 @@ namespace CSLight
         {
             // дамашнее задание: локальные максимумы: 
 
-            int[] array = {8,6,15,12,18,3,7,6,4,15,6,19,84,65,14,35,1,9,14,45,14,69,48,25,67,1,9,15,12,36};
+            int[] array = { 8, 6, 15, 12, 18, 3, 7, 6, 4, 15, 6, 19, 84, 65, 14, 35, 1, 9, 14, 45, 14, 69, 48, 25, 67, 1, 9, 15, 12, 36 };
             int minimalNumber = 0;
             int maximalNumber = 100;
             int minimalBorder = 0;
@@ -21,30 +21,25 @@ namespace CSLight
 
             if (array[minimalBorder] > array[minimalBorder + nearbyNumber])
             {
-                    Console.WriteLine("\nЛокальный максимум:");
-                    Console.Write(array[minimalBorder] + "\n");   
+                Console.WriteLine("\nЛокальный максимум:");
+                Console.Write(array[minimalBorder] + "\n");
             }
-           
+
             if (array[maximalBorder] > array[maximalBorder - nearbyNumber])
             {
-                
-                    Console.WriteLine("\nЛокальный максимум:");
-                    Console.Write(array[maximalBorder] + "\n");
+
+                Console.WriteLine("\nЛокальный максимум:");
+                Console.Write(array[maximalBorder] + "\n");
             }
 
-            for (int i = 0; i < array.Length; i++)
+            for (int i = 1; i < (array.Length - nearbyNumber); i++)
             {
 
-                if (i > minimalBorder && i < maximalBorder)
+                if (array[i] > array[i - nearbyNumber] && array[i] > array[i + nearbyNumber])
                 {
-
-                    if (array[i] > array[i - nearbyNumber] && array[i] > array[i + nearbyNumber])
-                    {
-                        Console.WriteLine("\nЛокальный максимум:");
-                        Console.WriteLine(array[i]);
-                    }
+                    Console.WriteLine("\nЛокальный максимум:");
+                    Console.WriteLine(array[i]);
                 }
-
             }
         }
     }
