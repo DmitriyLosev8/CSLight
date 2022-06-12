b8ed7c543aea10bb5d2922d37f37d098f59aee10
DmitriyLosev8 <dmitriylosev8@gmail.com>
2022-06-12 17:36:44 +0300

    работа с конкретными строками,столбцами

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -17,8 +17,10 @@ namespace CSLight
             int mult = 1;
             int line = 1;
             int column = 0;
+           
             for (int i = 0; i < array.GetLength(0); i++)
             {
+               
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
                     
