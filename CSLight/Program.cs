af9d72ea891e9bf36d41442ade4cc27daf822917
DmitriyLosev8 <dmitriylosev8@gmail.com>
2022-06-12 18:30:34 +0300

    работа с конкретными строками,столбцами

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -10,35 +10,33 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            // дамашнее задание: работа с конкретными строками,столбцами.
+            // домашнее задание: работа с конкретными строками,столбцами.
 
-            int[,] array = { { 8, 5, 9 }, {6,3,4 }, {8,7,2 } };
+            int[,] array = { { 8, 5, 9 },{ 6, 3, 4 },{ 8, 7, 2 } };
             int sum = 0;
             int mult = 1;
             int line = 1;
             int column = 0;
-           
-            for (int i = 0; i < array.GetLength(0); i++)
+
+           for (int i = 0; i < array.GetLength(1); i++)
             {
-               
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    
-                    if ( i == line)
-                    {
-                        sum += array[i, j];
-                    }
-                     
-                    if ( j == column)
-                    {
-                        mult *= array[i, j];
-                    }
-                    Console.Write(array[i, j] + " | ");   
-                }
-                Console.WriteLine("\n");
-            } 
+             sum += array[line, i];   
+            }
             Console.WriteLine("Сумма второй строки - " + sum);
+
+            for (int j = 0; j < array.GetLength(0); j++)
+            {
+                mult *= array[column, j];
+            }
             Console.WriteLine("Произведение первого столбца - " + mult + "\n");
+
+            for (int k = 0; k < array.GetLength(0); k++)
+            {
+                for (int l = 0; l < array.GetLength(1); l++)
+                {
+                    Console.Write(array[k, l] + " | ");
+                } Console.WriteLine("\n");
+            } 
         }
     }
 }
