2ba9c9046600f9de2b8ea44f1e7a63b220e33534
DmitriyLosev8 <dmitriylosev8@gmail.com>
2022-06-13 11:21:23 +0300

    наибольший элемент

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -10,33 +10,46 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            // домашнее задание: работа с конкретными строками,столбцами.
+            // домашнее задание: наибольший элемент:
 
-            int[,] array = { { 8, 5, 9 },{ 6, 3, 4 },{ 8, 7, 2 } };
-            int sum = 0;
-            int mult = 1;
-            int line = 1;
-            int column = 0;
+            int[,] array = { {7,9,5,6,47,4,8,9,2,3 }, {4,2,1,6,36,7,4,3,9,1 }, {7,9,8,15,2,6,4,1,9,5 },{1,3,2,5,7,89,4,6,5,8 }, {2,6,98,8,9,12,6,5,1,7 },{2,15,36,2,4,15,23,8,9,4 },
+                           {1,5,8,6,2,4,6,4,14,98 },{1,8,96,47,51,12,1,5,6,14 }, };
+            int maximumNumber = int.MinValue;
+            int newValueOfElement = 0;
 
-           for (int i = 0; i < array.GetLength(1); i++)
+            Console.WriteLine("Исходная матрица: \n");
+            
+            for (int i = 0; i < array.GetLength(0); i++)
             {
-             sum += array[line, i];   
-            }
-            Console.WriteLine("Сумма второй строки - " + sum);
-
-            for (int j = 0; j < array.GetLength(0); j++)
-            {
-                mult *= array[column, j];
-            }
-            Console.WriteLine("Произведение первого столбца - " + mult + "\n");
-
-            for (int k = 0; k < array.GetLength(0); k++)
-            {
-                for (int l = 0; l < array.GetLength(1); l++)
+                
+                for (int j = 0; j < array.GetLength(1); j++)
                 {
-                    Console.Write(array[k, l] + " | ");
-                } Console.WriteLine("\n");
-            } 
+                    
+                    if (maximumNumber < array[i, j])
+                    {
+                        maximumNumber = array[i, j];                    
+                    }
+                    Console.Write(array[i, j] + " | ");                   
+                }
+                Console.WriteLine("\n");
+            }
+            Console.WriteLine("Наибольший элемент матрицы - " + maximumNumber + "\n");
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                      
+                    if (array[i, j] == maximumNumber)
+                    {
+                        array[i, j] = newValueOfElement;
+                    }
+                    Console.Write(array[i, j] + " | ");
+                }
+                Console.WriteLine("\n");
+            }                   
+            Console.WriteLine("Новая матрица \n");
         }
     }
 }
