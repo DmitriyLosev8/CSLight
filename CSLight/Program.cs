25449fb306fc14f2eeb8b709c39b88d8110e1328
DmitriyLosev8 <dmitriylosev8@gmail.com>
2022-06-17 12:29:45 +0300

    подмассив повторейний чисел

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -13,42 +13,47 @@ namespace CSLight
 
             // домашнее задание: подмассив повторейний чисел:     
 
-            int[] array = { 2, 6, 4, 8, 8, 8, 5, 6, 4, 8, 6, 4, 4, 4, 4, 4, 4, 3, 2, 8};
+            int[] array = { 2, 6, 4, 8, 8, 8, 5, 6, 4, 8, 6, 4, 4, 4, 4, 4, 4, 3, 2, 8,2,4,12,67,4,87,12,5,6,6,};
             int count = 0;
-            int repeatNumber;
             int element;
             int maximumCount = int.MinValue;
-            int rrrr;
 
             Console.WriteLine("Весь массив - ");
             for (int i = 0; i < array.Length; i++)
             {
-                Console.Write(array[i] + " | "); 
+                Console.Write(array[i] + " | ");
             }
+
             for (int j = 1; j < array.Length; j++)
             {
                 if (array[j] == array[j - 1])
                 {
                     count++;
-                    element = array[j];
-                    Console.WriteLine("\nПовторяющийся элемент - " +  element);
-                    repeatNumber = count;
-                   
                     if (maximumCount < count)
                     {
                         maximumCount = count;
                     }
-                    rrrr = element;
                 }
-                
                 else if (array[j] != array[j - 1])
                 {
                     count = 0;
-                }       
+                }
             }
-            Console.WriteLine("Максимальное колличество повторений  - " + maximumCount);
-            //Console.WriteLine("\nПовторяющийся элемент - " + rrrr);
 
+            for (int j = 1; j < array.Length; j++)
+            {
+                if (array[j] == array[j - 1])
+                {
+                    count++;
+                    if (count == maximumCount)
+                    {
+                        element = array[j];
+                        Console.WriteLine("\n\nМаксимально часто повторяющийся элемент - " + element);
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine("\nМаксимальное колличество повторений  - " + maximumCount + "\n");
         }
     }
 }
