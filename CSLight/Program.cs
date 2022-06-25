8b210422f3074a6dd0629050343a583317469173
DmitriyLosev8 <dmitriylosev8@gmail.com>
2022-06-25 13:15:59 +0300

    Кадровый учёт:   недоделанное

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -10,47 +10,93 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
+            // Задание: Кадровый учёт:   недоделанное
 
-            // домашнее задание: сдвиг значений массива:
+            string[] fio = new string[0];
+            string[] post = new string[0];
+           // string noteToFio;
+            //string noteToPost = "";
+            string userInformation;
+            string userInput;
+            bool isWorking = true;
 
-            int[] array = { 1, 2, 3, 4 };
-            int userInput;
-            int tempNumber;
-            int count = 0;
-            int lowerBorder = 0;
-            int numberToCorrectBorder = 1;
-
-            Console.WriteLine("Исходный массив:");
             
-            for (int i = 0; i < array.Length; i++)
-            {
-                Console.Write(array[i] + " | ");
-            }
-           
-            Console.WriteLine("\n\nЧтобы сдвинуть значения элементов массива влево, введите число на сколько позиций будет сдвиг:");
-            userInput = Convert.ToInt32(Console.ReadLine());
 
-            while (count < userInput)
+            while (isWorking)
             {
-                tempNumber = array[lowerBorder];
+                Console.SetCursorPosition(45, 0);
+                Console.WriteLine("ПЕРЕД ВАМИ ПРОГРАММА КАДРОВОГО УЧЁТА");
+                Console.SetCursorPosition(0, 2);
+                Console.WriteLine("Нажмите 1, чтобы добавить досье\nНажмите 2, чтобы показать все досье\nНажмите 3, чтобы удалить какое-то досье\n" +
+                    "Нажмите 4, для поиска досье по фамилии\nНажмите 5, чтобы выйти\n\n");
+                userInput = Console.ReadLine();
                 
-                for (int j = numberToCorrectBorder; j < array.Length; j++)
+                switch (userInput)
                 {
-                    array[j - 1] = array[j];
+                    case "1":
+                        Console.WriteLine("Введите Ваши ФИО:");
+                        userInformation = Console.ReadLine();
+                        AddDossier(fio, userInformation);
+                        Console.WriteLine("Введите Вашe должность:");
+                        userInformation = Console.ReadLine();
+                        AddDossier(post, userInformation);
+
+                        Console.WriteLine("Вот список досье:\n");
+
+                        for (int i = 0; i < fio.Length; i++)
+                        {
+                            for (int j = 0; j < post.Length; j++)
+                            {
+                                Console.Write(fio[i] + post[j] + "-");   //(i + 1) + ") " + 
+                            }
+                        }
+
+                        break;
+                    case "2":
+                        Console.WriteLine("Вот список досье:\n");
+
+                        for (int i = 0; i < fio.Length; i++)
+                        {
+                            for (int j = 0; j < post.Length; j++)
+                            {
+                                Console.Write((i + 1) + ") " + fio[i] + post[j] + "-");
+                            }
+                        }
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        isWorking = false;
+                        break;
                 }
-                array[array.Length - 1] = tempNumber;
-                count++;
+                Console.ReadKey();
+                //Console.Clear();
+                Console.SetCursorPosition(45, 0);
+                Console.WriteLine("ПЕРЕД ВАМИ ПРОГРАММА КАДРОВОГО УЧЁТА");
+                Console.SetCursorPosition(0, 2);
+                Console.WriteLine("Нажмите 1, чтобы добавить досье\nНажмите 2, чтобы показать все досье\nНажмите 3, чтобы удалить какое-то досье\n" +
+                    "Нажмите 4, для поиска досье по фамилии\nНажмите 5, чтобы выйти\n\n");
+                userInput = Console.ReadLine();
+
+                //Console.ReadKey();
             }
-           
-            Console.WriteLine("Изменённый массив:");
-            
-            for (int i = 0; i < array.Length; i++)
-            {
-                Console.Write(array[i] + " | ");
-            }
-            
-            Console.WriteLine();
         }
+
+        static string[] AddDossier(string[] partOfDossier, string userInformation)
+        {
+            string[] tempPartOfDossier = new string[partOfDossier.Length + 1];
+
+            for (int i = 0; i < partOfDossier.Length; i++)
+            {
+                tempPartOfDossier[i] = partOfDossier[i];
+            }
+            tempPartOfDossier[tempPartOfDossier.Length - 1] = userInformation;
+            partOfDossier = tempPartOfDossier;
+            return partOfDossier;
+        }
+
     }
 }
 
