ebe59ebe3c08f977c021fe5b2ced959d3fed367b
DmitriyLosev8 <dmitriylosev8@gmail.com>
2022-06-26 23:02:16 +0300

    Кадровый учёт

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -14,13 +14,11 @@ namespace CSLight
 
             string[] fio = new string[0];
             string[] post = new string[0];
-           // string noteToFio;
-            //string noteToPost = "";
             string userInformation;
             string userInput;
             bool isWorking = true;
 
-            
+
 
             while (isWorking)
             {
@@ -30,38 +28,33 @@ namespace CSLight
                 Console.WriteLine("Нажмите 1, чтобы добавить досье\nНажмите 2, чтобы показать все досье\nНажмите 3, чтобы удалить какое-то досье\n" +
                     "Нажмите 4, для поиска досье по фамилии\nНажмите 5, чтобы выйти\n\n");
                 userInput = Console.ReadLine();
-                
+
                 switch (userInput)
                 {
                     case "1":
                         Console.WriteLine("Введите Ваши ФИО:");
                         userInformation = Console.ReadLine();
-                        AddDossier(fio, userInformation);
+                        fio = AddDossier(fio, userInformation);
                         Console.WriteLine("Введите Вашe должность:");
                         userInformation = Console.ReadLine();
-                        AddDossier(post, userInformation);
-
-                        Console.WriteLine("Вот список досье:\n");
-
-                        for (int i = 0; i < fio.Length; i++)
-                        {
-                            for (int j = 0; j < post.Length; j++)
-                            {
-                                Console.Write(fio[i] + post[j] + "-");   //(i + 1) + ") " + 
-                            }
-                        }
-
+                        post = AddDossier(post, userInformation);
                         break;
                     case "2":
                         Console.WriteLine("Вот список досье:\n");
 
+                       
+                        
                         for (int i = 0; i < fio.Length; i++)
-                        {
+                        { 
                             for (int j = 0; j < post.Length; j++)
                             {
-                                Console.Write((i + 1) + ") " + fio[i] + post[j] + "-");
+                                if (i == j)
+                                {
+                                    Console.Write((i + 1) + ") " + fio[i] + "-" + post[j] + ", ");
+                                }                              
                             }
                         }
+                        
                         break;
                     case "3":
                         break;
@@ -72,15 +65,7 @@ namespace CSLight
                         break;
                 }
                 Console.ReadKey();
-                //Console.Clear();
-                Console.SetCursorPosition(45, 0);
-                Console.WriteLine("ПЕРЕД ВАМИ ПРОГРАММА КАДРОВОГО УЧЁТА");
-                Console.SetCursorPosition(0, 2);
-                Console.WriteLine("Нажмите 1, чтобы добавить досье\nНажмите 2, чтобы показать все досье\nНажмите 3, чтобы удалить какое-то досье\n" +
-                    "Нажмите 4, для поиска досье по фамилии\nНажмите 5, чтобы выйти\n\n");
-                userInput = Console.ReadLine();
-
-                //Console.ReadKey();
+                Console.Clear();
             }
         }
 
@@ -96,6 +81,8 @@ namespace CSLight
             partOfDossier = tempPartOfDossier;
             return partOfDossier;
         }
+
+
 
     }
 }
