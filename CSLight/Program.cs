cca2916138823306c15cbbae94263946dd32c19f
DmitriyLosev8 <dmitriylosev8@gmail.com>
2022-06-29 18:23:16 +0300

    // Задание: UIElement:

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -10,142 +10,50 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            // Задание: Кадровый учёт: 
+            // Задание: UIElement:    
 
-            string[] fullNames = new string[0];
-            string[] posts = new string[0];
-            string userInput;
-            bool isWorking = true;
+            string healthBar = "";
+            int prosentsOfBar;
+            string manaBar = "";
+            string scaleOfBar = "";
 
-            while (isWorking)
-            {
-                Console.SetCursorPosition(45, 0);
-                Console.WriteLine("ПЕРЕД ВАМИ ПРОГРАММА КАДРОВОГО УЧЁТА");
-                Console.SetCursorPosition(0, 2);
-                Console.WriteLine("Нажмите 1, чтобы добавить досье\nНажмите 2, чтобы показать все досье\nНажмите 3, чтобы удалить какое-то досье\n" +
-                    "Нажмите 4, для поиска досье по фамилии\nНажмите 5, чтобы выйти\n\n");
-                userInput = Console.ReadLine();
-
-                switch (userInput)
-                {
-                    case "1":
-                        AddDossier(ref fullNames, ref posts); 
-                        break;
-                    case "2":
-                        ShowDossier(fullNames,posts);
-                        break;
-                    case "3":
-                        DeleteDossier(ref fullNames, ref posts);
-                        break;
-                    case "4":
-                        SearchOfFio(fullNames);
-                        break;
-                    case "5":
-                        isWorking = false;
-                        break;
-                }
-                Console.ReadKey();
-                Console.Clear();
-            }
+            Console.WriteLine("Введите сколько процентов жизни вы хотите отобразить:");
+            prosentsOfBar = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите 1 символ, которым будут отобрадаться жизни");
+            scaleOfBar = Console.ReadLine();
+            DrawBar(healthBar, prosentsOfBar, scaleOfBar);
+            Console.WriteLine("Введите сколько процентов маны вы хотите отобразить:");
+            prosentsOfBar = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите 1 символ, которым будет отображаться мана");
+            scaleOfBar = Console.ReadLine();
+            DrawBar(manaBar, prosentsOfBar, scaleOfBar);
         }
 
-        static void AddDossier(ref string[] fullNames, ref string[] posts)
+        static void DrawBar(string bar, int prosentsOfBar, string scaleOfBar)
         {
-            string userInformation;
-            Console.WriteLine("Введите Ваши ФИО:");
-            userInformation = Console.ReadLine();
-            fullNames = AddPartOfDossier(fullNames, userInformation);
-            Console.WriteLine("Введите Вашу должность:");
-            userInformation = Console.ReadLine();
-            posts = AddPartOfDossier(posts, userInformation);
-        }
+            string emptyScaleOfBar = " ";
+            int multiplicity = 10;
+            int fullBar = 100;
 
-        static string[] AddPartOfDossier(string[] partOfDossier, string userInformation)
-        {      
-            string[] tempPartOfDossier = new string[partOfDossier.Length + 1];
-
-            for (int i = 0; i < partOfDossier.Length; i++)
+            if (prosentsOfBar < multiplicity || prosentsOfBar > fullBar)
             {
-                tempPartOfDossier[i] = partOfDossier[i];
-            }
-            tempPartOfDossier[tempPartOfDossier.Length - 1] = userInformation;
-            partOfDossier = tempPartOfDossier;
-            return partOfDossier;
-        }
-
-        static void ShowDossier(string[] fullNames,string[] posts)
-        {
-            int numberToCorrectInput = 1;
-            Console.WriteLine("Вот список досье:\n");
-
-            for (int i = 0; i < fullNames.Length; i++)
-            {
-                Console.Write((i + numberToCorrectInput) + ") " + fullNames[i] + " - " + posts[i] + ", ");
-            }
-        }
-
-        static void DeleteDossier(ref string[] fullNames, ref string[] posts)
-        {
-            int numberOfDossierToDelete;
-            
-            Console.WriteLine("Какое по счёту досье вы хотите удалить?");
-            numberOfDossierToDelete = Convert.ToInt32(Console.ReadLine());
-
-            if (numberOfDossierToDelete > fullNames.Length || numberOfDossierToDelete > posts.Length)
-            {
-                Console.WriteLine("Такого досье нет, введите корректный номер досье");
-            }
+                Console.WriteLine("Введите корректное значение");
+            }       
             else
             {
-                fullNames = DeletePartOfDossier(fullNames, numberOfDossierToDelete);
-                posts = DeletePartOfDossier(posts, numberOfDossierToDelete);
-            }
-        }
+                Console.WriteLine("\nВот ваш Bar:\n");
 
-        static string[] DeletePartOfDossier(string[] partOfDossier, int numberOfDossierToDelete)
-        {
-            int numberToCorrectInput = 1;
-            string[] tempParOfDossier = new string[partOfDossier.Length - 1];
-
-            for (int i = 0; i < numberOfDossierToDelete - numberToCorrectInput; i++)
-            {
-                tempParOfDossier[i] = partOfDossier[i];
-            }
-
-            for (int i = numberOfDossierToDelete; i < partOfDossier.Length; i++)
-            {
-                tempParOfDossier[i - 1] = partOfDossier[i];
-            }
-            partOfDossier = tempParOfDossier;
-            return partOfDossier;
-        }
-
-        static void SearchOfFio(string[] fullNames)
-        {
-            string inputedLastname;
-            string lastname;
-            int numberOfDossier;
-            int numberToCorrectInput = 1;
-
-            Console.WriteLine("Введите фамилию:");
-            inputedLastname = Console.ReadLine();
-
-            for (int i = 0; i < fullNames.Length; i++)
-            {
-                lastname = fullNames[i];
-                string[] separateFullName = lastname.Split(' ');
-
-                for (int j = 0; j < separateFullName.Length; j++)
+                for (int i = 0; i < prosentsOfBar / multiplicity; i++)
                 {
-                    if (separateFullName[j] == inputedLastname)
-                    {
-                        numberOfDossier = i;
-                        
-                        Console.WriteLine("Вот номер досье и полное имя этого человека:\n");
-                        Console.Write((i + numberToCorrectInput) + ") " + fullNames[i]);  
-                    }
+                    bar += scaleOfBar;
                 }
-            }
+                
+                for (int i = prosentsOfBar / multiplicity; i < fullBar / multiplicity; i++)
+                {
+                    bar += emptyScaleOfBar;
+                }
+                Console.Write("[" + bar + "]\n");
+            }  
         }
     }
 }
