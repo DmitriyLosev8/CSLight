f3c56ad3ebe6d8b788238d6cd2754b8536eb3726
DmitriyLosev8 <dmitriylosev8@gmail.com>
2022-06-29 23:19:20 +0300

    Создание игры "Pac-Man"

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;                       //мы вбили это, чтобы мы могли работать с файлами внутри компа
 
 namespace CSLight
 {
@@ -10,50 +11,56 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            // Задание: UIElement:    
+            //Создание игры "Pac-Man" - Передвижение
 
-            string healthBar = "";
-            int prosentsOfBar;
-            string manaBar = "";
-            string scaleOfBar = "";
-
-            Console.WriteLine("Введите сколько процентов жизни вы хотите отобразить:");
-            prosentsOfBar = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите 1 символ, которым будут отобрадаться жизни");
-            scaleOfBar = Console.ReadLine();
-            DrawBar(healthBar, prosentsOfBar, scaleOfBar);
-            Console.WriteLine("Введите сколько процентов маны вы хотите отобразить:");
-            prosentsOfBar = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите 1 символ, которым будет отображаться мана");
-            scaleOfBar = Console.ReadLine();
-            DrawBar(manaBar, prosentsOfBar, scaleOfBar);
+            int pacmanX, pacmanY;
+            char[,] map = ReadMap("map1", out pacmanX, out pacmanY);  
+            
+                //координаты персонажа пакман  Чтобы правильно изначально задать координаты пакмана(карты ведь будут меняться) и он не оказался в стене
+                                     // надо указать эти координаты в файле, который считывает функция изначально 
+            DrawMap(map);
+            Console.SetCursorPosition(pacmanY, pacmanX);  //надо рисовать координаты наоборот.
+            Console.WriteLine("@");
         }
 
-        static void DrawBar(string bar, int prosentsOfBar, string scaleOfBar)
+        static void DrawMap(char[,] map)    
         {
-            string emptyScaleOfBar = " ";
-            int multiplicity = 10;
-            int fullBar = 100;
-
-            if (prosentsOfBar < multiplicity || prosentsOfBar > fullBar)
+            for (int i = 0; i < map.GetLength(0); i++)
             {
-                Console.WriteLine("Введите корректное значение");
-            }       
-            else
-            {
-                Console.WriteLine("\nВот ваш Bar:\n");
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    Console.Write(map[i, j]);  
+                }
+                Console.WriteLine();   
+            }
+        }
 
-                for (int i = 0; i < prosentsOfBar / multiplicity; i++)
+        static char[,] ReadMap(string mapName, out int pacmanX, out int pacmanY)    // добывили в параметры out переменные, потому что надо проинициализировать и перезаписывать в память  
+        {
+
+            pacmanX = 0;    //начальная инициализация, чтобы out мог выполниться. Потому что без этого прогрммма считает, что эти координаты не всегда могут быть полученны
+                             // (да, в теории там может быть ещё и  else
+            pacmanY = 0;
+
+            string[] newFile = File.ReadAllLines($"Maps/{mapName}.txt");   
+                                                                 
+            char[,] map = new char[newFile.Length, newFile[0].Length];   
+
+            for(int i = 0; i < map.GetLength(0); i++)   
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
                 {
-                    bar += scaleOfBar;
+                    
+                    map[i, j] = newFile[i][j];
+                    
+                    if (map[i, j] == '@')    // мы говорим, что если в массиве по двум координатам будет символ собаки, то  pacmanX = i;, а pacmanY = j;
+                    {                         // то есть надо поставить туда игрока (в фауле с картой, надо нарисовать этот символ, где будет игрок
+                        pacmanX = i;
+                        pacmanY = j;
+                    }
                 }
-                
-                for (int i = prosentsOfBar / multiplicity; i < fullBar / multiplicity; i++)
-                {
-                    bar += emptyScaleOfBar;
-                }
-                Console.Write("[" + bar + "]\n");
-            }  
+            }
+            return map;
         }
     }
 }
