630ea5ab8ef08b702fb43fad16ec255f1c45ebb4
DmitriyLosev8 <dmitriylosev8@gmail.com>
2022-07-01 12:05:03 +0300

    Shuffle

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;                       //мы вбили это, чтобы мы могли работать с файлами внутри компа
+
 
 namespace CSLight
 {
@@ -11,56 +11,31 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            //Создание игры "Pac-Man" - Передвижение
+            //Задание: Shuffle:
 
-            int pacmanX, pacmanY;
-            char[,] map = ReadMap("map1", out pacmanX, out pacmanY);  
+            int[] array = { 2, 6, 9, 4, 5, 6, 7, 3, 1, 9, 8, 5 };
+            Shuffle(ref array);  
+        }
+
+        static void Shuffle(ref int[] array)
+        {
+            int randomNumber;
+            int elementNumber;
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                randomNumber = random.Next(i);                          
+                elementNumber = array[i];
+                array[i] = array[randomNumber];
+                array[randomNumber] = elementNumber;
+            }
             
-                //координаты персонажа пакман  Чтобы правильно изначально задать координаты пакмана(карты ведь будут меняться) и он не оказался в стене
-                                     // надо указать эти координаты в файле, который считывает функция изначально 
-            DrawMap(map);
-            Console.SetCursorPosition(pacmanY, pacmanX);  //надо рисовать координаты наоборот.
-            Console.WriteLine("@");
-        }
-
-        static void DrawMap(char[,] map)    
-        {
-            for (int i = 0; i < map.GetLength(0); i++)
+            for (int i = 0; i < array.Length; i++)
             {
-                for (int j = 0; j < map.GetLength(1); j++)
-                {
-                    Console.Write(map[i, j]);  
-                }
-                Console.WriteLine();   
+                Console.Write(array[i] + " | ");
             }
-        }
-
-        static char[,] ReadMap(string mapName, out int pacmanX, out int pacmanY)    // добывили в параметры out переменные, потому что надо проинициализировать и перезаписывать в память  
-        {
-
-            pacmanX = 0;    //начальная инициализация, чтобы out мог выполниться. Потому что без этого прогрммма считает, что эти координаты не всегда могут быть полученны
-                             // (да, в теории там может быть ещё и  else
-            pacmanY = 0;
-
-            string[] newFile = File.ReadAllLines($"Maps/{mapName}.txt");   
-                                                                 
-            char[,] map = new char[newFile.Length, newFile[0].Length];   
-
-            for(int i = 0; i < map.GetLength(0); i++)   
-            {
-                for (int j = 0; j < map.GetLength(1); j++)
-                {
-                    
-                    map[i, j] = newFile[i][j];
-                    
-                    if (map[i, j] == '@')    // мы говорим, что если в массиве по двум координатам будет символ собаки, то  pacmanX = i;, а pacmanY = j;
-                    {                         // то есть надо поставить туда игрока (в фауле с картой, надо нарисовать этот символ, где будет игрок
-                        pacmanX = i;
-                        pacmanY = j;
-                    }
-                }
-            }
-            return map;
+            Console.WriteLine();
         }
     }
 }
