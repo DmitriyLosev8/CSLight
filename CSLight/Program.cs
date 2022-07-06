34c1cb3b10bcca65cc922e78aa168e8f10dcfcf4
DmitriyLosev8 <dmitriylosev8@gmail.com>
2022-07-06 18:13:17 +0300

    очередь в магазине

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -10,41 +10,29 @@ namespace CSLight
     internal class Program
     {
         static void Main(string[] args)
-        {      //Задание: Динамический массив продвинутый:    
+        {      //Задание: очередь в магазине:    
 
-            List<int> numbers = new List<int>();
-            ShowingAmauntOfNumbers(numbers);
+            Queue<int> queueToStore = new Queue<int>();
+            queueToStore.Enqueue(50);
+            queueToStore.Enqueue(80);
+            queueToStore.Enqueue(45);
+            queueToStore.Enqueue(36);
+            queueToStore.Enqueue(67);
+            queueToStore.Enqueue(90);
+            queueToStore.Enqueue(250);
+
+            int amountOfPurchases = 0;
+            ServiceOfClients(queueToStore, ref amountOfPurchases);
         }
 
-        static void ShowingAmauntOfNumbers(List<int> numbers)
+        static void ServiceOfClients(Queue<int> queueToStore, ref int amountOfPurchases)
         {
-            bool isWorking = true;
-            int userInput;
-            int amauntOfNumbers = 0;
-
-            while (isWorking)
+            while (queueToStore.Count > 0)
             {
-                Console.WriteLine("Введите число и программа его запомнит:");
-                userInput = Convert.ToInt32(Console.ReadLine());
-                numbers.Add(userInput);
-                Console.WriteLine("Нажмите  1, чтобы посмотреть сумму всех введённых вами чисел.\nНажмите 2 чтобы ввести ещё одно число.\nНажмитие 3,чтобы выйти.");
-                userInput = Convert.ToInt32(Console.ReadLine());
-
-                switch (userInput)
-                {
-                    case 1:
-                        foreach (int number in numbers)
-                        {
-                            amauntOfNumbers += number;
-                        }
-                        Console.WriteLine(amauntOfNumbers);
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        isWorking = false;
-                        break;
-                }
+                amountOfPurchases += queueToStore.Dequeue();
+                Console.WriteLine("Текущая сумма покупок всех клиентов - " + amountOfPurchases + ", нажмите любою клавишу, чтобы обслужить следующего клиента:");
+                Console.ReadKey(true);
+                Console.Clear();
             }
         }
     }
