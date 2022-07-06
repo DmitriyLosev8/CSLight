04e0ea7ce3316bf42516d1102de2490d3adac081
DmitriyLosev8 <dmitriylosev8@gmail.com>
2022-07-06 18:51:56 +0300

    Динамический массив продвинутый

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -31,7 +31,6 @@ namespace CSLight
                 {
                     case "s":
                         ShowSumNumbers(numbers);
-
                         break;
                     case "c":
                         AddNumbers(numbers, out userInput);
