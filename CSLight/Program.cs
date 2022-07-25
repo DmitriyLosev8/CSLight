5a44bd326da8c38d59037145aa217024729f0f5a
DmitriyLosev8 <dmitriylosev8@gmail.com>
2022-07-25 20:18:25 +0300

    Гладиаторские бои

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -11,100 +11,140 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            //Задание: Гладиаторские бои:       ДОДЕЛАТЬ
+            //Задание: Гладиаторские бои:       
 
-
-            bool readyToFight = true;
             Arena arena = new Arena();
-            arena.ShowAllWarriors();
-
-            // ВОТ ТУТ. Я создаю массив с типом Warrior, внутри него создаю уже классы, но по-факту они всё теже Warrior.
-
-
-
-
-
+            arena.PickWarrior();
         }
     }
-
 
     class Arena
     {
-        static bool readyToFight = true;
-        Warrior[] warriors = { new Giant("Гигант", 100, 500, 50, readyToFight), new Knight("Рыцарь", 50, 200, 30, readyToFight), new Wizard("Волшебник", 50, 150, 30, readyToFight) };
+        private static bool _readyToFight = true;
+        private Warrior[] _warriors = { new Giant("Гигант", 10, 300, 50, _readyToFight), new Knight("Рыцарь", 5, 200, 30, _readyToFight), new Wizard("Волшебник", 5, 150, 30, _readyToFight),
+            new Recruit("Рекрут",3,150,25,_readyToFight), new Officer("Офицер", 7,300,50,_readyToFight) };
 
         public void ShowAllWarriors()
         {
-            Console.WriteLine("Вот список всех бойцов:");
-            
-            for(int i = 0; i < warriors.Length; i++)
+            Console.WriteLine("Вот список всех бойцов:\n");
+
+            for (int i = 0; i < _warriors.Length; i++)
             {
                 Console.Write(i + 1 + " ");
-                warriors[i].ShowIndicators();
-
+                _warriors[i].ShowIndicators();
             }
         }
 
-        public void Fight()
+        public void PickWarrior()
         {
+            int leftWarriorIndex;
+            int rightWarriorIndex;
+            string userInput;
+            bool isSuccessfull;
+
+            Console.SetCursorPosition(45, 0);
+            Console.WriteLine("ДОБРО ПОЖАЛОВАТЬ НА АРЕНУ:");
+            Console.SetCursorPosition(0, 2);
             ShowAllWarriors();
-            while
+            Console.WriteLine("\nВыберете левого бойца:");
+            userInput = Console.ReadLine();
+            isSuccessfull = int.TryParse(userInput, out leftWarriorIndex);
+
+            if (isSuccessfull == true && leftWarriorIndex <= _warriors.Length)
+            {
+                Console.WriteLine("\nВыберете правого бойца:");
+                userInput = Console.ReadLine();
+                isSuccessfull = int.TryParse(userInput, out rightWarriorIndex);
+                Fight(isSuccessfull, leftWarriorIndex, rightWarriorIndex);
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели не число или бойца под таким номером нет");
+            }
         }
 
+        public void ShowFigth(int leftWarriorIndex, int rightWarriorIndex)
+        {
+            Console.SetCursorPosition(35, 0);
+            Console.WriteLine("Сначала выберете способность для левого бойца, потом для правого");
+            Console.SetCursorPosition(0, 2);
+            Console.WriteLine("Боец слева:");
+            Console.SetCursorPosition(55, 2);
+            Console.WriteLine("Боец справа:");
+            Console.SetCursorPosition(0, 5);
+            _warriors[leftWarriorIndex - 1].ShowIndicators();
+            Console.SetCursorPosition(55, 5);
+            _warriors[rightWarriorIndex - 1].ShowIndicators();
+        }
 
+        public void ApplySkill(int warriorIndex, int stepOfFight)
+        {
+            Console.SetCursorPosition(25, 7);
+            Console.WriteLine("Применить способность у бойца?");
+            Console.SetCursorPosition(35, 8);
+            Console.WriteLine("д - Да, н - нет");
+            string userInput = Console.ReadLine();
 
+            if (userInput == "д")
+            {
+                _warriors[warriorIndex - 1].UniqueSkill(stepOfFight);
+            }
+        }
 
-
-
+        public void Fight(bool isSuccessfull, int leftWarriorIndex, int rightWarriorIndex)
+        {
+            int stepOfFight = 0;
+           
+            if (isSuccessfull == true && rightWarriorIndex <= _warriors.Length && leftWarriorIndex != rightWarriorIndex)
+            {
+                Console.Clear();
+                while (_warriors[leftWarriorIndex - 1].Health > 0 && _warriors[rightWarriorIndex - 1].Health > 0 && _warriors[leftWarriorIndex - 1].ReadyToFight == true && _warriors[rightWarriorIndex - 1].ReadyToFight == true)
+                {
+                    stepOfFight++;
+                    ShowFigth(leftWarriorIndex, rightWarriorIndex);
+                    ApplySkill(leftWarriorIndex, stepOfFight);
+                    _warriors[rightWarriorIndex - 1].TakeDamage(_warriors[leftWarriorIndex - 1].Damage);
+                    ApplySkill(rightWarriorIndex, stepOfFight);
+                    _warriors[leftWarriorIndex - 1].TakeDamage(_warriors[rightWarriorIndex - 1].Damage);
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели не число или бойца под таким номером нет/его уже выбрали");
+            }
+        }
     }
-    class Warrior 
+
+    class Warrior
     {
         protected string Name;
         protected int Armor;
-        protected bool ReadyToFight;
 
+        public bool ReadyToFight { get; protected set; }
         public int Health { get; protected set; }
         public int Damage { get; protected set; }
-
-        //public Warrior(string name, int armor, int health, int damage, bool readyToFight)
-        //{
-        //    Name = name;
-        //    Armor = armor;
-        //    Health = health;
-        //    Damage = damage;
-        //    ReadyToFight = true;
-        //}
+        public int UniqueFeature { get; protected set; }
 
         public void ShowIndicators()
         {
-            Console.WriteLine($"{Name} иммет {Damage} урона. Жизней - {Health}, брони - {Armor}");
+            Console.WriteLine($"{Name}, {Damage} - урона. Жизней - {Health}, брони - {Armor}");
         }
 
-        public virtual void UniqueSkill(int unqueInfo)
-        {
-
-        }
-
-        public virtual void SecondUniqueSkill()
-        {
-
-        }
-
+        public virtual void UniqueSkill(int uniqueInfo) { }
 
         public void TakeDamage(int damage)
         {
             Health -= damage - Armor;
         }
-
-        public void LosePartOfDamage(int manaHit)
-        {
-            Damage -= manaHit;
-        }
     }
 
     class Giant : Warrior
     {
-        public Giant(string name, int armor, int health, int damage, bool readyToFight) //: base(name, armor, health, damage, readyToFight)
+        private int _coolDown = 6;
+
+        public Giant(string name, int armor, int health, int damage, bool readyToFight)
         {
             Name = name;
             Armor = armor;
@@ -115,30 +155,23 @@ namespace CSLight
 
         public override void UniqueSkill(int stepOfFight)
         {
-            int coolDown = 5;
             Console.WriteLine("SuperHit + 30 к урону");
 
-            if (stepOfFight % coolDown == 0)
+            if (stepOfFight % _coolDown == 0)
             {
                 Damage += 30;
             }
         }
-
-
-        //public void SuperHit(int stepOfFight)
-        //{
-        //    int coolDown = 5;
-
-        //    if (stepOfFight % coolDown == 0)
-        //    {
-        //        Damage += 30;
-        //    }
-        //}
     }
 
     class Knight : Warrior
     {
-        public Knight(string name, int armor, int health, int damage, bool readyToFight) //: base(name, armor, health, damage, readyToFight)
+        private int _possibilityToSkill = 1;
+        private int _lowerBorder = 0;
+        private int _upperBorder = 5;
+        private int _chance;
+
+        public Knight(string name, int armor, int health, int damage, bool readyToFight)
         {
             Name = name;
             Armor = armor;
@@ -147,59 +180,39 @@ namespace CSLight
             ReadyToFight = true;
         }
 
-        public override void UniqueSkill(int damage)
+        public override void UniqueSkill(int stepOfFight)
         {
-            Console.WriteLine("RepairTakenDamage - 20% вероятность восстановления полученного урона");
-            int lowerBorder = 0;
-            int upperBorder = 5;
-            int possibility = 1;
+            Console.WriteLine("ExtraDamage - 20% вероятность увеличить урон на 15");
             Random random = new Random();
-            int chance = random.Next(lowerBorder, upperBorder);
+            _chance = random.Next(_lowerBorder, _upperBorder);
 
-            if (chance == possibility)
+            if (_chance == _possibilityToSkill)
             {
-                Health += damage - Armor;
+                Damage += 15;
             }
         }
     }
 
     class Wizard : Warrior
     {
-        public int _mana;
-        public Wizard(string name, int armor, int health, int damage, bool readyToFight) //: base(name, armor, health, damage, readyToFight)
+        private int _priceOfAplly = 25;
+
+        public Wizard(string name, int armor, int health, int damage, bool readyToFight)
         {
             Name = name;
             Armor = armor;
             Health = health;
             Damage = damage;
             ReadyToFight = true;
-            _mana = 100;
-
+            UniqueFeature = 100;
         }
 
-        //public override void SecondUniqueSkill()
-        //{
-        //    Console.WriteLine("GetExtraDamage - урон + 10");
-        //    int priceOfAplly = 25;
-        //    int manaHit = 10;
-
-        //    if (_mana >= priceOfAplly)
-        //    {
-        //        Damage += manaHit;
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine("Недостаточно маны");
-        //    }
-        //}
-
-        public override void SecondUniqueSkill()
+        public override void UniqueSkill(int stepOfFight)
         {
-            Console.WriteLine("GetExtraHealth - жизни + 20");
-            int priceOfAplly = 25;
-
-            if (_mana >= priceOfAplly)
+            if (UniqueFeature >= _priceOfAplly)
             {
+                Console.WriteLine("GetExtraHealth - жизни + 20");
+                UniqueFeature -= _priceOfAplly;
                 Health += 20;
             }
             else
@@ -211,7 +224,7 @@ namespace CSLight
 
     class Recruit : Warrior
     {
-        public Recruit(string name, int armor, int health, int damage, bool readyToFight) //: base(name, armor, health, damage, readyToFight)
+        public Recruit(string name, int armor, int health, int damage, bool readyToFight)
         {
             Name = name;
             Armor = armor;
@@ -220,7 +233,7 @@ namespace CSLight
             ReadyToFight = true;
         }
 
-        public override void SecondUniqueSkill()
+        public override void UniqueSkill(int stepOfFight)
         {
             Console.WriteLine("GiveUp - боец сдался");
             ReadyToFight = false;
@@ -229,7 +242,9 @@ namespace CSLight
 
     class Officer : Warrior
     {
-        public Officer(string name, int armor, int health, int damage, bool readyToFight) //: base(name, armor, health, damage, readyToFight)
+        private int _coolDown = 8;
+
+        public Officer(string name, int armor, int health, int damage, bool readyToFight)
         {
             Name = name;
             Armor = armor;
@@ -238,18 +253,17 @@ namespace CSLight
             ReadyToFight = true;
         }
 
-        public void DoubleDamage(int stepOfFight)
+        public override void UniqueSkill(int stepOfFight)
         {
-            int coolDown = 8;
-
-            if (stepOfFight % coolDown == 0)
+            if (stepOfFight % _coolDown == 0)
             {
+                Console.WriteLine("DoubleDamage - двойной урон");
                 Damage *= 2;
             }
         }
     }
-
 }
+
 
 
 
