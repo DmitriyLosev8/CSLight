1e7d1a920ba835d764ac6be3ab2813201254d6b0
DmitriyLosev8 <dmitriylosev8@gmail.com>
2022-07-28 17:22:53 +0300

    ДОРАБОТАТЬ

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -11,146 +11,272 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            //Задание: Колода карт:       
+            //Задание: Конфигуратор пассажирских поездов:   ДОРАБОТАТЬ
 
-            PlayingTable playingTable = new PlayingTable();
-            playingTable.ShowMenu();
+            //ManagerOfRailwayStation managerOfRailwayStation = new ManagerOfRailwayStation();
+            //managerOfRailwayStation.ShowMenu();
+
+            Carriage carriage = new Carriage();
+            carriage.AddPassangers(123);
+
         }
     }
 
-    class PlayingTable
+    class ManagerOfRailwayStation
     {
-        private bool _isPlaying = true;
-        private Player _player = new Player();
-        private CardDeck _cardDeck = new CardDeck();
+        private bool isWorking = true;
+        private RailwayStation _railwayStation = new RailwayStation();
+        private Train _train = new Train();
+        private Direction _direction = new Direction("Направление",0);
 
         public void ShowMenu()
         {
-            _cardDeck.AddCard();
 
-            while (_isPlaying)
+            while (isWorking == true)
             {
                 int userNumber;
                 Console.SetCursorPosition(45, 0);
-                Console.WriteLine("Добро пожаловать за игральный стол");
-                Console.WriteLine("Чтобы взять карты, нажмите 1\nЧтобы посмотреть взятые карты, нажмите 2\nЧтобы выйти, нажмите любую другую цифру");
+                Console.WriteLine("Добро пожаловать в программу управления ЖД вокзалом");
+                Console.SetCursorPosition(60, 2);
+                Console.WriteLine("Вот информация о текущем рейсе:");
+                Console.SetCursorPosition(0, 3);
+                Console.WriteLine("Чтобы создать направление, нажмите 1\nЧтобы продать билеты на это направление, нажмите 2\nЧобы сформировать поез, нажмите 3,\n" +
+                    "Чтобы отправить поезд, нажмите 4\nЧтобы выйти, нажмите 5");
                 string userInput = Console.ReadLine();
                 bool isSuccessfull = int.TryParse(userInput, out userNumber);
-                Play(isSuccessfull, userNumber);
+                PickFunction(isSuccessfull, userNumber);
+
             }
         }
 
-        private void Play(bool isSuccessfull, int userNumber)
+        private void PickFunction(bool isSuccessfull, int userNumber)
         {
-            if (_isPlaying == true)
+            if (isSuccessfull == true)
             {
-                if (userNumber == 1)
+                switch (userNumber)
                 {
-                    TakeCards(isSuccessfull, userNumber);
-                }
-                else if (userNumber == 2)
-                {
-                    _player.ShowTakenCards();
-                }
-                else
-                {
-                    _isPlaying = false;
+                    case 1:
+                        _direction = _railwayStation.CreateADirection();
+                        //ShowAllInfo();
+                        break;
+                    case 2:
+                        _railwayStation.SellTickets(_direction);
+                        //ShowAllInfo();
+                        break;
+                    case 3:
+                        _railwayStation.FormATrain();
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        isWorking = false;
+                        break;
                 }
             }
             else
             {
-                Console.WriteLine("Вы ввели не число");
+                Console.WriteLine("Вы ввели не число.");
             }
             Console.ReadKey();
             Console.Clear();
+            ShowAllInfo();
         }
 
-        private void TakeCards(bool isSuccessfull, int userNumber)
+        public void ShowAllInfo()
         {
-            Console.WriteLine("Сколько карт вы хотите взять?");
-            string userInput = Console.ReadLine();
-            isSuccessfull = int.TryParse(userInput, out userNumber);
+            _direction.ShowInfo();
+            _railwayStation.ShowPassengers();
+            _train.ShowInfo();
+        }
+    }
 
-            if (isSuccessfull == true)
+    class RailwayStation
+    {
+        public int CountOfPassengers { get; private set; }
+        public bool isOnStation { get; private set; } = true;
+
+        private int _minimalNumberOfPassengers = 10;
+        private int _maximumNumberOfPassengers = 200;
+        private int _minimalPriceOfTicket = 50;
+        private int _maximumPriceOfTicket = 100;
+        private int _money = 1000;
+        private List<Passanger> _waitingHall = new List<Passanger>();
+
+        public void SendTrain()
+        {
+            isOnStation = false;
+        }
+
+        public void GetReadyToSendAnotherTrain()
+        {
+            isOnStation = true;
+        }
+
+
+        public Direction CreateADirection()
+        {
+            Direction direction = new Direction("Направление", 0);
+
+            if (isOnStation == true)
             {
-                while (userNumber > 0)
-                {
-                    Card giveAwayCard = _cardDeck.GiveAwayCard(userNumber);
-                    _player.TakeCard(giveAwayCard);
-                    userNumber--;
-                }
+                Random random = new Random();
+                Console.WriteLine("Введите направление в формате: Город - Город:");
+                string userDirection = Console.ReadLine();
+                direction = new Direction(userDirection, random.Next(_minimalPriceOfTicket, _maximumPriceOfTicket));
             }
             else
             {
-                Console.WriteLine("Вы ввели не число");
+                Console.WriteLine("Направление уже создано, сформируйте поезд и отправьте его.");
+            }
+            return direction;
+        }
+
+        public void SellTickets(Direction direction)
+        {
+            Random random = new Random();
+            CountOfPassengers = random.Next(_minimalNumberOfPassengers, _maximumNumberOfPassengers);
+            _waitingHall.Clear();
+           
+            while (CountOfPassengers > 0)
+            {          
+                Passanger passanger = new Passanger();
+                passanger.BuyATicket(direction.Ticket);
+                _money += direction.Ticket;
+                _waitingHall.Add(passanger);
+                CountOfPassengers--;
             }
         }
+
+        public void ShowPassengers()
+        {
+            Console.SetCursorPosition(60, 4);
+            if (_waitingHall.Count > 0)
+            {   
+                Console.WriteLine("По этому направлению поедут " + _waitingHall.Count + " пассажиров");
+            }
+            else
+            {
+                Console.WriteLine("Продайте билеты");
+            }  
+        }
+       
+        public void FormATrain()
+        {
+            if(_waitingHall.Count > 0)
+            {
+                CountOfPassengers = _waitingHall.Count; 
+                Train train = new Train();
+                train.FillATrain(CountOfPassengers);
+            }
+            else
+            {
+                Console.WriteLine("Продайте сначала билеты.");
+            }
+           
+        }
+
     }
 
-    class CardDeck
+    class Direction
     {
-        private List<Card> _cards = new List<Card>();
-        private string[] _suits = { "Пик", "Черва", "Бубна", "Трефа" };
-        private int[] _values = { 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-        public void AddCard()
+        public string Title { get; private set; }
+        public int Ticket { get; private set; }
+
+        public Direction(string title, int ticket)
         {
-            for (int i = 0; i < _suits.Length; i++)
-            {
-                for (int j = 0; j < _values.Length; j++)
-                {
-                    _cards.Add(new Card(_suits[i], _values[j]));
-                }
-            }
+            Title = title;
+            Ticket = ticket;
         }
 
-        public Card GiveAwayCard(int userNumber)
+        public void ShowInfo()
         {
-            Card giveAwayCard = new Card("Отдаваемая карта", 0);
-            if (userNumber <= _cards.Count)
-            {
-                giveAwayCard = _cards[0];
-                _cards.RemoveAt(0);
-            }
-            return giveAwayCard;
+            Console.SetCursorPosition(60, 3);
+            Console.WriteLine($"Направление - {Title}, стоимость билета - {Ticket}");
+            //Console.SetCursorPosition(0, 3);
         }
     }
 
-    class Player
+    class Train
     {
-        private List<Card> _takenCards = new List<Card>();
+        private List<Passanger> _fullTrain = new List<Passanger>();
+        //private List<Passanger> _сarriages = new List<Passanger>(_roominessOfCarriages);
+        private static int _roominessOfCarriages  = 20;
 
-        public void TakeCard(Card giveAvayCard)
+        public int CountOfCarriages { get; private set; } = 0; 
+
+        private List<Passanger> AddPassangers(int countOfPassengers)
         {
-            if (giveAvayCard.Suit != "Отдаваемая карта")
+            List<Passanger> _сarriages = new List<Passanger>(_roominessOfCarriages);
+            while (countOfPassengers > 0)
             {
-                _takenCards.Add(giveAvayCard);
+                _сarriages.Add(new Passanger());
+                countOfPassengers--;
+            }
+            return _сarriages;
+        }
+
+
+        public void FillATrain(int countOfPassengers)
+        {
+            while (countOfPassengers > 0)
+            {
+                CountOfCarriages++;
+                List<Passanger> _сarriages = new List<Passanger>(_roominessOfCarriages);
+                _сarriages = AddPassangers(countOfPassengers);
+                _fullTrain.AddRange(_сarriages);
+
+                //Carriage carriage = new Carriage();
+                //carriage.AddPassangers(countOfPassengers);
+                //_fullTrain.Add(carriage);
+                //countOfPassengers-= carriage.Roominess;
             }
         }
 
-        public void ShowTakenCards()
+        public void ShowInfo()
         {
-            Console.WriteLine("Вот список взятых карт:");
-
-            for (int i = 0; i < _takenCards.Count; i++)
+            Console.SetCursorPosition(60, 5);
+            
+            if (_fullTrain.Count > 0)
             {
-                Console.WriteLine(_takenCards[i].Value + " " + _takenCards[i].Suit);
+                Console.WriteLine($"Поезд состоит из {_fullTrain.Count % _roominessOfCarriages} вагонов");
             }
+            else
+            {
+                Console.WriteLine("Cформируйте поезд");
+            }      
         }
     }
 
-    class Card
+    //class Carriage
+    //{
+    //    public static int Roominess { get; private set; } = 20;
+    //    private List<Passanger> _passangers = new List<Passanger>(Roominess);
+
+    //public void AddPassangers(int countOfPassengers)
+    //{
+    //    while (countOfPassengers > 0)
+    //    {
+    //        _passangers.Add(new Passanger());
+    //        //countOfPassengers--;
+    //    }
+    //}
+
+
+
+    //}
+
+    class Passanger
     {
-        public string Suit { get; private set; }
+        public int Money { get; private set; } = 500;
 
-        public int Value { get; private set; }
-
-        public Card(string suit, int value)
+        public void BuyATicket(int ticket)
         {
-            Suit = suit;
-            Value = value;
+            Money -= ticket;
         }
     }
+
+
 }
 
 
