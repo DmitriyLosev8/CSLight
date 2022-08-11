2167510e00fea04243f1402e751d65bd67f94045
DmitriyLosev8 <dmitriylosev8@gmail.com>
2022-08-11 15:31:48 +0300

    автосервис

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -11,75 +11,236 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            //Задание: автосервис:   
-
-            Client client = new Client();
-            Console.WriteLine(client._car.IdOfDefect);
-
+            //Задание: автосервис:        
+           
+            CarService carService = new CarService();
+            carService.StartToWork();
         }
     }
 
     class CarService
     {
-        private int _money;
+        private int _money = 20000;
+        private int _priceOfService;
         private int _priceOfWork;
-        private string _service;
-        private int _idOfService;
+        private int _fine = 150;
         private List<Detail> _storage = new List<Detail>();
-        private Queue<Client> _clients = new Queue<Client>();
+        public List<Client> _clients = new List<Client>();
+        private bool _isWorking = true;
 
-        public void AddDetail()
-        {
-            int necessaryCount = 10;
-
-            for (int i = 0; i < necessaryCount; i++)
-            {
-                _storage.Add(new Detail());        
-            }
-        }
-
-        public void AddClients()
-        {
-            int countOfClients = 5;
-
-            for (var i = 0; i < countOfClients; i++)
-            {
-                _clients.Enqueue(new Client());
-            }
-        }
+        public int _accuracyOfWork { get; private set; } = 0;
 
         public void StartToWork()
         {
             AddDetail();
             AddClients();
 
+            while (_isWorking == true)
+            {
+                FormPrices(_clients[0]);
+                ShowAllInfo(_clients[0], _priceOfService);
+                PrepareToRepair(_clients[0]);
+                _clients.RemoveAt(0);
 
+                if (_clients.Count == 0)
+                {
+                    Console.WriteLine("Клиенты закончились");
+                    _isWorking = false;
+                }
+            }
         }
 
-        public void Repair()
+        private void FormPrices(Client client)
         {
+            switch (client._car.IdOfDefect)
+            {
+                case 1:
+                    _priceOfWork = 150;
+                    break;
+                case 2:
+                    _priceOfWork = 75;
+                    break;
+                case 3:
+                    _priceOfWork = 250;
+                    break;
+            }
+            _priceOfService = _priceOfWork + (_priceOfWork * 2);
+        }
 
+        private void ShowAllInfo(Client client, int priceOfService)
+        {
+            Console.WriteLine($"Денег в автосервисе - {_money}\nПоломка у текущего автомобиля - {client._car.Defect}\n" +
+                $"Итоговая цена починки состоит из цены за деталь + цены за работу(в два раза меньше цены за деталь) - {priceOfService}");
+            Console.SetCursorPosition(0, 18);
+            Console.WriteLine("Склад автосервиса:");
+
+            for (int i = 0; i < _storage.Count; i++)
+            {
+                Console.WriteLine(_storage[i].Id + " - " + _storage[i].Name);
+            }
+            Console.SetCursorPosition(0, 7);
+        }
+
+        private void PrepareToRepair(Client client)
+        {
+            string userInput;
+            bool isSuccessfull;
+            int userNumber;
+
+            Console.WriteLine("Введите id детали, которую надо заменить, чтобы найти её на складе произвести ремонт\nЧтобы выйти, нажмите 9");
+            userInput = Console.ReadLine();
+            isSuccessfull = int.TryParse(userInput, out userNumber);
+            FindADetail(client, isSuccessfull, userNumber);
+        }
+
+        private void FindADetail(Client client, bool isSuccessfull, int userNumber)
+        {
+            if (isSuccessfull == true)
+            {
+                int indexOfDetail;
+                int firstFoundDetail = 0;
+
+                if (userNumber == 9)
+                {
+                    _isWorking = false;
+                }
+                else
+                {
+                    for (int i = 0; i < _storage.Count; i++)
+                    {
+                        if (_storage[i].Id == userNumber && client._car.IdOfDefect == userNumber)
+                        {
+                            firstFoundDetail++;
+
+                            if (firstFoundDetail == 1)
+                            {
+                                indexOfDetail = i;
+                                _accuracyOfWork = 1;
+                                Repair(client, indexOfDetail);
+                            }
+                        }
+                    }
+
+                    if (firstFoundDetail == 0)
+                    {
+                        Console.WriteLine("Такой детали нет на складе. Или вы ввели не тот id.\nОтказать клиенту и заплатить штраф (нажмите 1)\n" +
+                            "Установить ему первую неподходящую деталь (нажмите 2)");
+                        string userInput = Console.ReadLine();
+                        isSuccessfull = int.TryParse(userInput, out userNumber);
+
+                        if (isSuccessfull == true)
+                        {
+                            if (userNumber == 1)
+                            {
+                                Console.WriteLine("Вы заплатили штраф");
+                                GiveAwayMoney(_fine, true);
+                            }
+                            else if (userNumber == 2)
+                            {
+                                indexOfDetail = 0;
+                                Console.WriteLine("Вы установили ему неподходящую деталь. Надейтесь, что клиент не заметит");
+                                _accuracyOfWork = 2;
+                                Repair(client, indexOfDetail);
+                                int financialDamage = _priceOfService * 5;
+                                bool isNoticed = client.CheckRepairedCar(_accuracyOfWork, financialDamage);
+                                GiveAwayMoney(financialDamage, isNoticed);
+                            }
+                        }
+                    }
+                }
+            }
+            else if (isSuccessfull == false)
+            {
+                Console.WriteLine("Вы ввели не число");
+            }
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        private void Repair(Client client, int indexOfDetail)
+        {
+            client.GiveAwayMoney(_priceOfService);
+            GetMoney(_priceOfService);
+            _storage.RemoveAt(indexOfDetail);
+        }
+
+        private void GiveAwayMoney(int fine, bool isNoticed)
+        {
+            if (_isWorking == true)
+            {
+                _money -= fine;
+            }
+        }
+
+        private void GetMoney(int priceOfService)
+        {
+            _money += priceOfService;
+        }
+
+        private void AddDetail()
+        {
+            int necessaryCount = 10;
+
+            for (int i = 0; i < necessaryCount; i++)
+            {
+                _storage.Add(new Detail());
+                System.Threading.Thread.Sleep(150);
+            }
+        }
+
+        private void AddClients()
+        {
+            int countOfClients = 5;
+
+            for (var i = 0; i < countOfClients; i++)
+            {
+                _clients.Add(new Client());
+                System.Threading.Thread.Sleep(150);
+            }
         }
     }
 
-    class Client 
+    class Client
     {
         private int _money = 5000;
 
         public Car _car { get; private set; } = new Car();
-        
+
         public void GetMoney(int financialDamage)
         {
             _money += financialDamage;
         }
-        
-        public void GiveAwayMoney(int priceOfDetail, int priceOfWork)
+
+        public void GiveAwayMoney(int priceOfService)
         {
-            _money -= priceOfDetail + priceOfWork;
+            _money -= priceOfService;
+        }
+
+        public bool CheckRepairedCar(int accuracyOfWork, int financialDamage)
+        {
+            bool isNoticed = false;
+            Random random = new Random();
+            int chanceToNoticeFalse;
+            int minimalBorder = 0;
+            int maximalBorder = 3;
+            chanceToNoticeFalse = random.Next(minimalBorder, maximalBorder);
+
+            if (chanceToNoticeFalse == 0 || accuracyOfWork == 1)
+            {
+                Console.WriteLine("Клиент вам благодарен");
+            }
+            else
+            {
+                if (accuracyOfWork == 2)
+                {
+                    Console.WriteLine("Клиент заметил, что вы поставили ему не ту деталь. У вас спишется 5 стоимостей услуги из-за ущерба клиенту");
+                    GetMoney(financialDamage);
+                    isNoticed = true;
+                }
+            }
+            return isNoticed;
         }
     }
-
-
 
     class Car
     {
@@ -102,7 +263,7 @@ namespace CSLight
                     Defect = "Закончилось масло";
                     break;
                 case 3:
-                    Defect = "Разбилось левое зеркало";
+                    Defect = "Разбилось зеркало заднего вида";
                     break;
             }
         }
